dc8c15fd2beb408c5c7db7feb599e6d34ce30477
AJ P <10paineaj@gmail.com>
2021-06-23 21:53:25 -0600

    Clean up and bubble chart updates with dropdown selection

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,90 +1,64 @@
 function init() {
-    //Create initial bar chart
-    var traceBar = [{
-        type: 'bar',
-        orientation: 'h',
-        x: [1, 2, 4, 5, 6, 2, 1, 6, 8, 10],
-        y: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-        text: ['test1', 'test2', 'test3']
-    }];
-
-    var layout = {
-        title: 'Top 10 Belly Bacteria by Sample',
-    }
-
-    Plotly.newPlot("bar", traceBar, layout);
-
-    //Create initial bubble chart
-    var traceBubble = [{
-        type: 'scatter',
-        mode: 'markers',
-        marker: {
-            color: 'rgb(17, 157, 255)',
-            size: 20,
-            line: {
-                color: 'black',
-                width: 2,
-            },
-        },
-        x: [1, 2, 4, 5, 6, 2, 1, 6, 8, 10],
-        y: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-        text: ['test1', 'test2', 'test3']
-    }];
-
-    var layoutBubble = {
-        title: `Sample X`,
-    };
-
-    Plotly.newPlot("bubble", traceBubble, layoutBubble);
-
-
-
-    //Read in JSON data and console log it
+    //Load JSON
     d3.json('../samples.json').then(data => {
+        //Print json to console
         console.log(data);
-
-        //Create variables to plot
-        const samples = data.names;
-        const metaData = data.metadata;
-        const sampleData = data.samples;
-        let otu_ids = sampleData[1].otu_ids;
-        console.log(metaData);
-        console.log(sampleData);
-        console.log(samples);
-        console.log(otu_ids);
-
-        //var metaDssata = data.samples.map(sample => sample.sample_values);
-        //var otu_ids = data.samples.map(sample => sample.otu_ids);
-        //var otu_labels = data.samples.map(sample =>sample.otu_labels);
-
-        //Append names of each sample to selDataset html id
-        samples.forEach((sample) => {d3.select('#selDataset').append('option').text(sample).property('value', sample)
-        });
-
-        //Filter data by sample
-        function filterSamples(sample) {
-            return samples.id === sample;
+        //Set-Up initial hBar plot with empty data
+        var traceBar = [{
+            type: 'bar',
+            orientation: 'h',
+            x: [],
+            y: [],
+            text: [],
+        }];
+        //Set-up Layout
+        var layout = {
+            title: 'Top 10 Belly Bacteria for Sample',
         };
-
-        function filterMetaData(sample) {
-            return data.metadata.id == sample;
+        //Set-up initial bubble plot with empty data
+        var traceBubble = [{
+            type: 'scatter',
+            mode: 'markers',
+            marker: {
+                color: 'red',
+                size: 20,
+                line: {
+                    color: 'black',
+                    width: 2,
+                },
+            },
+            x: [1, 2, 4, 5, 6, 2, 1, 6, 8, 10],
+            y: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+            text: ['test1', 'test2', 'test3']
+        }];
+        var layoutBubble = {
+            title: `Sample X`,
+            xaxis: {title: 'OTU ID'},
+            yaxis: {title: 'Number of OTU Present'}
         };
-
+        //Use plotly to display charts
+        Plotly.newPlot("bar", traceBar, layout);
+        Plotly.newPlot("bubble", traceBubble, layoutBubble);
+        //Pull first sample name
+        firstSample = data.names[0];
+        //Run plotting and panel functions with first sample
+        barPlot(firstSample);
+        demoPanel(firstSample);
+        bubblePlot(firstSample);
+        //Append names of each sample to selDataset html id for dropdown menu
+        data.names.forEach((sample) => {d3.select('#selDataset').append('option').text(sample).property('value', sample)});
         //Add event to listen to each time a different sample is selected
         d3.selectAll("#selDataset").on("change", getData);
-
-        //Write function to pull data for selected sample
+        //Write function to pull data for selected sample and call functions to update plots & panels
         function getData() {
             var dropDownMenu = d3.select("#selDataset");
             var sampleID = dropDownMenu.property("value");
-            demoPanel(`${sampleID}`);
             barPlot(`${sampleID}`);
+            bubblePlot(sampleID);
+            demoPanel(`${sampleID}`);
         };
-
-    })
-
-    function barPlot(sampleID) {
-        d3.json('../samples.json').then(data => {
+        //Function to update bar plot
+        function barPlot(sampleID) {
             //Set data.samples array to variable for clarity
             let sampleData = data.samples;
             //Filter sampleData to match function input
@@ -104,36 +78,54 @@ function init() {
             let revLabel = slicedLabel.reverse();
             let revText = slicedText.reverse();
             //Map labels scaling and appearance on
-            let labels = revLabel.map(label => `OTU ${label}`)
+            let labels = revLabel.map(label => `OTU ${label}`);
             //Create new title with sample name
-            newLayout = {
+            let newLayout = {
                 title: `Top 10 Belly Bacteria for sample ${sampleID}`,
-            }
+            };
             //Restyle horizontal bar chart
             Plotly.restyle("bar", "x", [revData]);
             Plotly.restyle("bar", "y", [labels]);
             Plotly.restyle("bar", "text", [revText]);
             Plotly.relayout("bar", newLayout);
-            console.log(slicedData)
-        });
-    };
-
-    function bubblePlot(sampleID) {
-        //Set data.samples array to variable for clarity
-        let sampleData = data.samples;
-        //Filter sampleData to match function input
-        let matchedSample = sampleData.filter(sample => sample.id == sampleID);
-        //Pull filtered data's otu_id from array
-        let match = matchedSample[0];
-        //Define variables to update plot
-        let matchData = match.sample_values;
-        let matchLabel = match.out_ids;
-        let matchText = match.out_labels;
-        //Restyle bubble chart
-    };
-
-    function demoPanel(sampleID) {
-        d3.json('../samples.json').then(data => {
+        };
+        //Function to update bubblePlot
+        function bubblePlot(sampleID) {
+            //Set data.samples array to variable for clarity
+            let sampleData = data.samples;
+            //Filter sampleData to match function input
+            let matchedSample = sampleData.filter(sample => sample.id == sampleID);
+            //Pull filtered data's otu_id from array
+            let match = matchedSample[0];
+            //Define variables to update plot
+            let matchData = match.sample_values;
+            let matchLabel = match.otu_ids;
+            let matchText = match.otu_labels;
+            //Restyle size and color for selected sample
+            let markerUpdate = {
+                marker: {
+                    color: matchLabel,
+                    size: matchData,
+                    opacity: 0.5,
+                    line:{
+                        color:'black',
+                        width: 1,
+                    }
+                }
+            }
+            //Create new layout with title
+            let newLayout = {
+                title: `OTU ID and Quantity for Sample ${sampleID}`
+            }
+            //Restyle bubble chart
+            Plotly.restyle("bubble", "x", [matchLabel]);
+            Plotly.restyle("bubble", "y", [matchData]);
+            Plotly.restyle("bubble", "text", [matchText]);
+            Plotly.restyle("bubble", "marker", [markerUpdate]);
+            Plotly.relayout("bubble", newLayout);
+        };
+        //Function to update demographic panel
+        function demoPanel(sampleID) {
             //Select html object to append metadata to
             let panel = d3.select("#sample-metadata");
             //Set data.metadata to variable for clarity
@@ -143,14 +135,11 @@ function init() {
             //Even though metadata is filtered, must pull filtered data from array
             let sampleMeta0 = sampleMeta[0];
             //Must reset panel html each time or it will append new selection below previous selection
-            panel.html('')
+            panel.html('');
             //Append each key, value pairing from the filtered array
             Object.entries(sampleMeta0).forEach(([key, value]) => {panel.append('h6').text(`${key}: ${value}`);
             });
-        });
-    };
-
-    demoPanel('946');
+        };
+    });
 };
-
 init();

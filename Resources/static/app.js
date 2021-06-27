ab74409028a2327c4e07840e93c78dc7b6a3cf02
AJ P <10paineaj@gmail.com>
2021-06-27 12:51:37 -0600

    Add gauge chart with indicator line

--- Resources/static/app.js
+++ Resources/static/app.js
@@ -37,15 +37,42 @@ function init() {
             xaxis: {title: 'OTU ID'},
             yaxis: {title: 'Number of OTU Present'}
         };
+        //Set-up initial bubble plot with empty data
+        var traceGauge = [{
+                domain: { x: [0, 1], y: [0, 1] },
+                value: 20,
+                title: { text: "Belly Button Washing Frequency"},
+                type: "indicator",
+                mode: "gauge+number",
+                gauge: {
+                    axis: {range: [0, 10]},
+                    bar: {color: "darkgrey"},
+                    steps: [
+                        {range: [0, 1], color: "darkred"},
+                        {range: [1, 2], color: "red"},
+                        {range: [2, 3], color: "darkorange"},
+                        {range: [3, 4], color: "orange"},
+                        {range: [4, 5], color: "gold"},
+                        {range: [5, 6], color: "yellow"},
+                        {range: [6, 7], color: "greenyellow"},
+                        {range: [7, 8], color: "lawngreen"},
+                        {range: [8, 9], color: "limegreen"},
+                        {range: [9, 10], color: "green"},
+                    ],
+                }
+            }];
+        var layoutGauge = { width: 600, height: 500, margin: { t: 0, b: 0 } };
         //Use plotly to display charts
         Plotly.newPlot("bar", traceBar, layout);
         Plotly.newPlot("bubble", traceBubble, layoutBubble);
+        Plotly.newPlot('gauge', traceGauge, layoutGauge);
         //Pull first sample name
         firstSample = data.names[0];
         //Run plotting and panel functions with first sample
         barPlot(firstSample);
         demoPanel(firstSample);
         bubblePlot(firstSample);
+        gaugePlot(firstSample);
         //Append names of each sample to selDataset html id for dropdown menu
         data.names.forEach((sample) => {d3.select('#selDataset').append('option').text(sample).property('value', sample)});
         //Add event to listen to each time a different sample is selected
@@ -57,6 +84,7 @@ function init() {
             barPlot(`${sampleID}`);
             bubblePlot(sampleID);
             demoPanel(`${sampleID}`);
+            gaugePlot(sampleID);
         };
         //Function to update bar plot
         function barPlot(sampleID) {
@@ -82,7 +110,7 @@ function init() {
             let labels = revLabel.map(label => `OTU ${label}`);
             //Create new title with sample name
             let newLayout = {
-                title: `Top 10 Belly Bacteria for sample ${sampleID}`,
+                title: `Top 10 Belly Bacteria for Test Subject: ${sampleID}`,
             };
             //Restyle horizontal bar chart
             Plotly.restyle("bar", "x", [revData]);
@@ -102,13 +130,9 @@ function init() {
             let matchData = match.sample_values;
             let matchLabel = match.otu_ids;
             let matchText = match.otu_labels;
-            //Restyle size and color for selected sample
-            var markerUpdate = {
-                marker: {color: 'red'}
-            }
             //Create new layout with title
             let newLayout = {
-                title: `OTU ID and Quantity for Sample ${sampleID}`
+                title: `OTU ID and Quantity for Test Subject: ${sampleID}`
             };
             //Restyle bubble chart
             Plotly.restyle("bubble", "x", [matchLabel]);
@@ -133,6 +157,29 @@ function init() {
             //Append each key, value pairing from the filtered array
             Object.entries(sampleMeta0).forEach(([key, value]) => {panel.append('h6').text(`${key}: ${value}`);
             });
+        };
+        //Function to update gauge plot
+        function gaugePlot(sampleID) {
+            //Set data.samples array to variable for clarity
+            let metaData = data.metadata;
+            //Filter sampleData to match function input
+            let matchedSample = metaData.filter(sample => sample.id == sampleID);
+            //Pull filtered data's otu_id from array
+            let match = matchedSample[0];
+            //Set variables equal to data from match for plotting
+            let washFreq = match.wfreq;
+            //Needle Indicator
+            //Set angle to 180 - wfreq * (180/# of sections in gauge)
+            //Needle starts at 180 deg (180 deg = 0 belly washes)
+            let angle = 180 - +match.wfreq * 180/10
+            let radius = 0.5
+            //SOHCAHTOA for coordinates
+            let x = radius*Math.cos(angle*Math.PI/180)
+            let y = radius*Math.sin(angle*Math.PI/180)
+            //Restyle horizontal bar chart
+            Plotly.restyle("gauge", "value", [washFreq]);
+            console.log(x);
+            console.log(y);
         };
     });
 };
